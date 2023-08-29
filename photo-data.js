262878b1b3f1d927ac47f8caa99c058fdd36a0df
xraxus <xraxus@gmail.com>
2023-08-29 15:18:52 +0200

    first commit

---
+++ photo-data.js
@@ -0,0 +1,197 @@
+export const photoData = [
+    {
+        tags: ["jedzonko"],
+        image: "jedzonko-1.jpg",
+        alt: "Pies jedzący marchewkę",
+    },
+    {
+        tags: ["mała", "jedzonko"],
+        image: "mala-jedzonko-1.jpg",
+        alt: "Pies malutki jedzący z miski",
+    },
+    {
+        tags: ["mała", "słodka"],
+        image: "mala-slodka-1.jpg",
+        alt: "Pies trzymany za rękach przez właściciela",
+    },
+    {
+        tags: ["mała", "słodka"],
+        image: "mala-slodka-2.jpg",
+        alt: "Pies leżacy na brzuchu, trzyma łapki przed sobą i patrzy w kamerę",
+    },
+    {
+        tags: ["mała", "słodka"],
+        image: "mala-slodka-3.jpg",
+        alt: "Pies leżacy na brzuchu, trzyma łapki przed sobą i patrzy w kamerę",
+    },
+    {
+        tags: ["mała", "słodka", "głupiutka"],
+        image: "mala-slodka-glupiutka-1.jpg",
+        alt: "Pies malutki szczekający na stokrotki",
+    },
+    {
+        tags: ["mała", "słodka", "odpoczynek"],
+        image: "mala-slodka-odpoczynek-1.jpg",
+        alt: "Pies mały, leżacy na kanapie",
+    },
+    {
+        tags: ["mała", "słodka", "odpoczynek"],
+        image: "mala-slodka-odpoczynek-2.jpg",
+        alt: "Pies mały, leżacy na kanapie",
+    },
+    {
+        tags: ["mała", "słodka", "odpoczynek"],
+        image: "mala-slodka-odpoczynek-3.jpg",
+        alt: "Pies mały, leżacy na kanapie",
+    },
+    {
+        tags: ["mała", "słodka", "odpoczynek"],
+        image: "mala-slodka-odpoczynek-4.jpg",
+        alt: "Pies mały, leżacy na kanapie",
+    },
+    {
+        tags: ["mała", "słodka", "odpoczynek", "głupiutka"],
+        image: "mala-slodka-odpoczynek-glupiutka-1.jpg",
+        alt: "Pies malutki, leży na łóżku i się rozciąga",
+    },
+    {
+        tags: ["mała", "słodka", "odpoczynek", "głupiutka"],
+        image: "mala-slodka-odpoczynek-glupiutka-2.jpg",
+        alt: "Pies malutki, leży na łóżku i się uśmiecha",
+    },
+    {
+        tags: ["mała", "słodka", "odpoczynek", "głupiutka"],
+        image: "mala-slodka-odpoczynek-glupiutka-3.jpg",
+        alt: "Pies malutki, leży na łóżku i się patrzy ciekawskim wzrokiem",
+    },
+    {
+        tags: ["mała", "słodka", "odpoczynek", "głupiutka"],
+        image: "mala-slodka-odpoczynek-glupiutka-4.jpg",
+        alt: "Pies malutki, leży na łóżku, rozciąga się i głupiutko uśmiecha",
+    },
+    {
+        tags: ["mała", "słodka", "spacer"],
+        image: "mala-slodka-spacer-1.jpg",
+        alt: "Pies na trawie, patrzy się w dal",
+    },
+    {
+        tags: ["mała", "słodka", "zabawa"],
+        image: "mala-slodka-zabawa-1.jpg",
+        alt: "Pies leży na podłodze i trzyma w łapach piłkę",
+    },
+    {
+        tags: ["odpoczynek"],
+        image: "odpoczynek-1.jpg",
+        alt: "Pies odpoczywa na podłodze przy murku",
+    },
+    {
+        tags: ["odpoczynek"],
+        image: "odpoczynek-2.jpg",
+        alt: "Pies odpoczywa na łóżku",
+    },
+    {
+        tags: ["odpoczynek", "słodka", "mood"],
+        image: "odpoczynek-3.jpg",
+        alt: "Pies odpoczywa na skraju łóżka",
+    },
+    {
+        tags: ["odpoczynek", "więcej niż jedno zwierzę"],
+        image: "odpoczynek-duet-1.jpg",
+        alt: "Psy odpoczywają na materacu, patrzą się w lewą stronę",
+    },
+    {
+        tags: ["odpoczynek", "więcej niż jedno zwierzę"],
+        image: "odpoczynek-duet-2.jpg",
+        alt: "Psy odpoczywają obok siebie, zwinięte w kulki",
+    },
+    {
+        tags: ["odpoczynek", "głupiutka"],
+        image: "odpoczynek-glupiutka-1.jpg",
+        alt: "Pies z głupim wyrazem twarzy patrzy się w kamerę",
+    },
+    {
+        tags: ["odpoczynek", "głupiutka"],
+        image: "odpoczynek-glupiutka-2.jpg",
+        alt: "Pies rozwalony na niepościelonym łóżku patrzy się w kamerę",
+    },
+    {
+        tags: ["odpoczynek", "mała", "słodka"],
+        image: "odpoczynek-mala-slodka-1.jpg",
+        alt: "Pies malutki, słodko sobie śpi",
+    },
+    {
+        tags: ["odpoczynek", "mała", "słodka"],
+        image: "odpoczynek-mala-slodka-2.jpg",
+        alt: "Pies malutki, słodko sobie śpi",
+    },
+    {
+        tags: ["odpoczynek", "mała", "słodka"],
+        image: "odpoczynek-mala-slodka-3.jpg",
+        alt: "Pies malutki, słodko sobie śpi",
+    },
+    {
+        tags: ["odpoczynek", "mood"],
+        image: "odpoczynek-mood-1.jpg",
+        alt: "Pies zmęczony leży na czerwonym kocyku",
+    },
+    {
+        tags: ["odpoczynek", "mood", "mała"],
+        image: "odpoczynek-mood-mala-1.jpg",
+        alt: "Pies zmęczony leży pod drzwiami",
+    },
+    {
+        tags: ["odpoczynek", "słodka"],
+        image: "odpoczynek-slodka-1.jpg",
+        alt: "Pies leży pod łóżkiem w legowisku",
+    },
+    {
+        tags: ["odpoczynek-spacer-1"],
+        image: "odpoczynek-spacer-1.jpg",
+        alt: "Pies leży na trawie i śpi",
+    },
+    {
+        tags: ["odpoczynek", "zabawa", "głupiutka"],
+        image: "odpoczynek-zabawa-glupiutka-1.jpg",
+        alt: "Pies wygina się na łóżku ze swoją zabawką",
+    },
+    {
+        tags: ["słodka", "mała"],
+        image: "slodka-mala-2.jpg",
+        alt: "Pies malutki co siedzi na fotelu gamera",
+    },
+    {
+        tags: ["spacer"],
+        image: "spacer-1.jpg",
+        alt: "Pies na spacerku",
+    },
+    {
+        tags: ["spacer"],
+        image: "spacer-2.jpg",
+        alt: "Pies i właściciel przed spacerem, ubrani w płaszcze przeciwdeszczowe",
+    },
+    {
+        tags: ["spacer"],
+        image: "spacer-3.jpg",
+        alt: "Pies i właścicielka na stacji kolejowej",
+    },
+    {
+        tags: ["spacer", "mała"],
+        image: "spacer-mala-1.jpg",
+        alt: "Pies malutki na spacerze",
+    },
+    {
+        tags: ["spacer", "zabawa", "głupiutka", "więcej niż jedno zwierzę"],
+        image: "spacer-zabawa-trio-glupiutka-1.jpg",
+        alt: "Pies szalenie bawiący się z dwoma innymi pieskami",
+    },
+    {
+        tags: ["zabawa", "głupiutka"],
+        image: "zabawa-glupiutka-1.jpg",
+        alt: "Pies z głupią minką bawi się zieloną zabawką",
+    },
+    {
+        tags: ["zabawa", "mała", "słodka"],
+        image: "zabawa-mala-slodka-1.jpg",
+        alt: "Pies malutki patrzy się w kamerę i obok niego jest piłka",
+    },
+];
