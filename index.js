6038da68a5617018835425176ddf32da98e61d16
xraxus <xraxus@gmail.com>
2023-08-30 13:31:49 +0200

    Complete rendering of the photo to the site

--- index.js
+++ index.js
@@ -2,23 +2,85 @@ import { photoData } from "./photo-data.js";
 
 const tagRadios = document.getElementById("tag-radios");
 const getImageBtn = document.getElementById("get-image-btn");
+const photoModal = document.getElementById("photo-modal");
+const photoModalInner = document.getElementById("photo-modal-inner");
+const photoModalCloseBtn = document.getElementById("photo-modal-close-btn");
 
-renderTagRadios(photoData);
+tagRadios.addEventListener("change", highlightCheckedOption);
+getImageBtn.addEventListener("click", renderDogPhoto);
+photoModalCloseBtn.addEventListener("click", closePhotoModal);
+
+document.addEventListener("click", function (event) {
+    if (
+        event.target != photoModal &&
+        event.target != getImageBtn &&
+        !photoModal.contains(event.target)
+    ) {
+        closePhotoModal();
+    }
+});
+
+function highlightCheckedOption(event) {
+    const radios = document.getElementsByClassName("radio");
+    for (let radio of radios) radio.classList.remove("highlight");
+    document
+        .getElementById(event.target.id)
+        .parentElement.classList.add("highlight");
+}
+
+function closePhotoModal() {
+    photoModal.style.display = "none";
+}
+
+function renderDogPhoto() {
+    const photoObject = getSinglePhoto();
+    photoModalInner.innerHTML = `
+        <img 
+        class="dog-img"
+        src="./img/${photoObject.image}"
+        alt="${photoObject.alt}"
+        >
+    `;
+    photoModal.style.display = "flex";
+}
+
+function getSinglePhoto() {
+    const photoArray = getMatchingPhotosArray();
+
+    if (photoArray.length === 1) {
+        return photoArray[0];
+    } else {
+        return photoArray[Math.floor(Math.random() * photoArray.length)];
+    }
+}
+
+function getMatchingPhotosArray() {
+    document.querySelector('input[type="radio"]:checked');
+    const selectedTag = document.querySelector(
+        'input[type="radio"]:checked'
+    ).value;
+
+    const matchingPhotoArray = photoData.filter(function (photo) {
+        return photo.tags.includes(selectedTag);
+    });
+
+    return matchingPhotoArray;
+}
 
 function renderTagRadios(photoData) {
     let radioItemsHTML = ``;
     const tags = getTagArray(photoData);
     for (let tag of tags) {
         radioItemsHTML += `
-        <div class="radio">
-            <label for="${tag}">${tag}</label>
-            <input 
-            type="radio"
-            id="${tag}"
-            value="${tag}"
-            name="tags">
-        </div>
-        `;
+            <div class="radio">
+                <label for="${tag}">${tag}</label>
+                <input 
+                type="radio"
+                id="${tag}"
+                value="${tag}"
+                name="tags">
+                </div>
+                `;
     }
 
     tagRadios.innerHTML = radioItemsHTML;
@@ -31,6 +93,7 @@ function getTagArray(photoData) {
             if (!tagArray.includes(tag)) tagArray.push(tag);
         }
     }
-    console.log(tagArray);
     return tagArray;
 }
+
+renderTagRadios(photoData);
